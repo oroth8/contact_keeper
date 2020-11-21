7bcc53a3c675ee451f990b2032f6768ace8bfeec
Owen Roth <rothowen27@gmail.com>
2020-11-21 15:03:58 -0600

    added route functionality and authrization

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../models/Users');
@@ -10,8 +11,14 @@ const User = require('../models/Users');
 // @route GET api/auth
 // @desc Get logged in user
 // @access private
-router.get('/', (req, res) => {
-    res.send("get logged in user");
+router.get('/', auth, async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
 });
 
 // @route POST api/auth
@@ -35,9 +42,23 @@ router.post('/', [
         const isMatch = await bcrypt.compare(password, user.password);
 
         if(!isMatch){
-            return res.status(400).json
+            return res.status(400).json({msg: 'Invalid Credentials'});
         }
-    }catch (err){}
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 3600
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
+    }catch (err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -49,7 +49,7 @@ router.post('/', [
         }, (err, token) => {
             if (err) throw err;
             res.json({ token });
-        })
+        });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
