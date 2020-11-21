afb4ad21ec5287cd0217b73ece4cda4c9e484d8f
Owen Roth <rothowen27@gmail.com>
2020-11-21 13:28:08 -0600

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check')
+
+const User = require('../models/Users');
 
 // @route GET api/auth
 // @desc Get logged in user
@@ -11,7 +17,10 @@ router.get('/', (req, res) => {
 // @route POST api/auth
 // @desc Auth user and get token
 // @access public
-router.post('/', (req, res) => {
+router.post('/', [
+    check('email', "Please include a valid email").isEmail(),
+    check('password', "Password is required")
+], (req, res) => {
     res.send("logged in user");
 });
 
