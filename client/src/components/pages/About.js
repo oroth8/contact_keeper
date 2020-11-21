af9e16b7adee422e03bc573efba8b4da438878e2
Owen Roth <rothowen27@gmail.com>
2020-11-21 16:13:49 -0600

    added react and navbar

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About This App</h1>
+            <p className='my-1'>
+                This is a full stack app for keeping contacts.
+            </p>
+            <p className='bg-dark p'>
+                <strong>Version 1.0.0</strong>
+            </p>
+        </div>
+    )
+}
+
+export default About;
