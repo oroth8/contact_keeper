9f7c1df4e45437eaa0f71f0396e9d62d2fb340bf
Owen Roth <rothowen27@gmail.com>
2020-11-21 13:28:57 -0600

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -20,8 +20,24 @@ router.get('/', (req, res) => {
 router.post('/', [
     check('email', "Please include a valid email").isEmail(),
     check('password', "Password is required")
-], (req, res) => {
-    res.send("logged in user");
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email, password} = req.body;
+
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({msg: "Invalid Credentials"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json
+        }
+    }catch (err){}
 });
 
 
