569335b477baca9ad9d3920ddb326228d4bbc42e
Owen Roth <rothowen27@gmail.com>
2020-11-22 10:40:14 -0600

    added auth context, register form, login form, and alert feature

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -9,3 +9,11 @@ export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT= 'REMOVE_ALERT';
 export const CLEAR_CURRENT = "CLEAR_CURRENT";
 export const SET_CURRENT = 'SET_CURRENT';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGOUT = 'LOGOUT';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
